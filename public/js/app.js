28f860e6eb222f019ce60c0e372e4b5db3da5150
Adrian Zawada <azawada93@gmail.com>
2019-12-22 15:16:08 +0100

    Add votes, answer and views counter to question item

--- public/js/app.js
+++ public/js/app.js
@@ -49592,8 +49592,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\laravel-qa\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel-qa\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /opt/lampp/htdocs/laravel-qa/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /opt/lampp/htdocs/laravel-qa/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
