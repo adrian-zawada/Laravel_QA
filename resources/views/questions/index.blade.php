e0768995812004cc75178f0f42e6e117d8454a3d
adrian-zawada <azawada93@gmail.com>
2019-12-30 03:02:00 +0100

    Hidden the button edit and delete if user is not a owner of question record

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,13 +37,19 @@
                                 <div class="d-flex algin-items-center">
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
-                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
 
-                                        <form class="form-delete" method="POST" action="{{ route('questions.destroy', $question->id) }}">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">DELETE</button>
-                                        </form>
+                                        @if (Auth::user()->can('update-question', $question))
+                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endif
+
+                                        @if (Auth::user()->can('delete-question', $question))
+                                            <form class="form-delete" method="POST" action="{{ route('questions.destroy', $question->id) }}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">DELETE</button>
+                                            </form>
+                                        @endif
+
                                     </div>
                                 </div>
                                     <p class="lead">
