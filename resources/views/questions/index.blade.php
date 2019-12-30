842f38cbbdecad3c3ad1a561e655e81b4588d750
adrian-zawada <azawada93@gmail.com>
2019-12-30 16:29:16 +0100

    Implement authorization using Policy in view

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,13 +37,19 @@
                                 <div class="d-flex algin-items-center">
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
-                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
 
-                                        <form class="form-delete" method="POST" action="{{ route('questions.destroy', $question->id) }}">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">DELETE</button>
-                                        </form>
+                                        @can ('update', $question))
+                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endif
+
+                                        @can('delete', $question))
+                                            <form class="form-delete" method="POST" action="{{ route('questions.destroy', $question->id) }}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">DELETE</button>
+                                            </form>
+                                        @endcan
+
                                     </div>
                                 </div>
                                     <p class="lead">
