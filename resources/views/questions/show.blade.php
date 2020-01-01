e8534634bfd7b001e73f0e4df89fe98e2fa91b0b
adrian-zawada <azawada93@gmail.com>
2020-01-02 00:00:18 +0100

    Create view of show and display answers in question

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -20,5 +20,25 @@
             </div>
         </div>
     </div>
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2>{{ $question->answers_count . " " . str_plural('Answer', $question->answers_count)}}</h2>
+                    </div>
+                    <hr>
+                    @foreach($question->answers as $answer)
+                        <div class="media">
+                            <div class="media-body">
+                                {!! $answer->body_html !!}
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
