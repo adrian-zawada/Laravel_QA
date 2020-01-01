4f161e634b1640d96e694aca18d1abb7fd74c8a9
adrian-zawada <azawada93@gmail.com>
2020-01-01 03:29:00 +0100

    Rename answers column to ansers_count in the questions table

--- app/Answer.php
+++ app/Answer.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getBodyHtmlAttribute ()
+    {
+        return \Parsedown::instance()->text($this->body);
+    }
 }
