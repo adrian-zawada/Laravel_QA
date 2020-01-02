7ddb8661da4ef6c5ce307d31c53defd284e49658
adrian-zawada <azawada93@gmail.com>
2020-01-02 01:05:10 +0100

    Display question detail, answer detail and reduced queries

--- app/Answer.php
+++ app/Answer.php
@@ -30,4 +30,9 @@ class Answer extends Model
         });
 
     }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -16,6 +16,17 @@
 
                 <div class="card-body">
                     {!! $question->body_html !!}
+                    <div class="float-right">
+                        <span class="text-muted">Answered {{ $question->created_date }}</span>
+                        <div class="media mt-2">
+                            <a href="{{ $question->user->url }}" class="pr-2">
+                                <img src="{{ $question->user->avatar }}" alt="">
+                            </a>
+                            <div class="media-body mt-1">
+                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -32,6 +43,17 @@
                         <div class="media">
                             <div class="media-body">
                                 {!! $answer->body_html !!}
+                                <div class="float-right">
+                                    <span class="text-muted">Answered {{ $answer->created_date }}</span>
+                                    <div class="media mt-2">
+                                        <a href="{{ $answer->user->url }}" class="pr-2">
+                                            <img src="{{ $answer->user->avatar }}" alt="">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <hr>
