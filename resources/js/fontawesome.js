e0fbef2c2ba6540946ade7cf6b0fd3e5974c1d3a
adrian-zawada <azawada93@gmail.com>
2020-01-02 01:43:45 +0100

    Install Font Awesome 5

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,7 @@
+import fontawesome from '@fortawesome/fontawesome';
+import faCareUp from '@fortawesome/fontawesome-free-solid';
+import faCareDown from '@fortawesome/fontawesome-free-solid';
+import faStar from '@fortawesome/fontawesome-free-solid';
+import faCheck from '@fortawesome/fontawesome-free-solid';
+
+fontawesome.library.add([faCareUp, faCareDown, faCheck, faStar]);
