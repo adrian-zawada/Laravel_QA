d7e75366228944654228679ec1865d819ad8452c
adrian-zawada <azawada93@gmail.com>
2020-01-05 03:08:08 +0100

    Build a validation message in view of answer body

--- resources/views/answers/_create.blade.php
+++ resources/views/answers/_create.blade.php
@@ -6,10 +6,15 @@
                     <h3>Your Answer</h3>
                 </div>
                 <hr>
-                <form action="" method="post">
+            <form action="{{ route('questions.answers.store', $question->id)}}" method="post">
                     @csrf
                     <div class="form-group">
-                        <textarea class="form-control" name="body" rows="7"></textarea>
+                    <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" name="body" rows="7"></textarea>
+                    @if ($errors->has('body'))
+                        <div class="invalid-feedback">
+                        <strong>{{ $errors->first('body') }}</strong>
+                        </div>
+                    @endif
                     </div>
                     <div class="form-group">
                         <button type="submit" class="btn btn-lg btn-outline-primary">Submit</button>
