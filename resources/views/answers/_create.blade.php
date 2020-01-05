ce3a0fd2bb80eaecc04b09211b0dcc92ff3a9ae4
adrian-zawada <azawada93@gmail.com>
2020-01-05 02:23:17 +0100

    Make a view of create answer and include in view

---
+++ resources/views/answers/_create.blade.php
@@ -0,0 +1,21 @@
+<div class="row mt-4">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    <h3>Your Answer</h3>
+                </div>
+                <hr>
+                <form action="" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <textarea class="form-control" name="body" rows="7"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-lg btn-outline-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
