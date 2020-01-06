2121ab2962a8fd016f36fe0c1db5c23b655d4f80
adrian-zawada <azawada93@gmail.com>
2020-01-06 23:03:24 +0100

    I checked the correctness of editing response and corrected the small bug in answer controller

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -53,7 +53,7 @@ class AnswersController extends Controller
             'body' => 'required',
         ]));
 
-        return redirect()->route('questions.show', $question->slug->with('success', "Your answer has been updated"));
+        return redirect()->route('questions.show', $question->slug)->with('success', "Your answer has been updated");
     }
 
     /**
