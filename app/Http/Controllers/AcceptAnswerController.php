3ed88869f3abbc54078fa024514ecd34230781c2
adrian-zawada <azawada93@gmail.com>
2020-01-07 23:34:01 +0100

    Authorize the accept answer button

--- app/Http/Controllers/AcceptAnswerController.php
+++ app/Http/Controllers/AcceptAnswerController.php
@@ -9,7 +9,10 @@ class AcceptAnswerController extends Controller
 {
     public function __invoke(Answer $answer)
     {
+        $this->authorize('accept', $answer);
+
         $answer->question->acceptBestAnswer($answer);
+
         return back();
     }
 }
