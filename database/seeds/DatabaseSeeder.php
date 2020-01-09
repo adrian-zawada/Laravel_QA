0ba82dc39cdda5dfc9a224d6f44dca4c9260426f
adrian-zawada <azawada93@gmail.com>
2020-01-09 22:47:08 +0100

    Make a new seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Question;
 use App\Answer;
 use Illuminate\Database\Seeder;
 
@@ -12,14 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 3)->create()->each(function($u) {
-            $u->questions()
-              ->saveMany(
-                  factory(App\Question::class, rand(1,5))->make()
-              )
-              ->each(function($q) {
-                $q->answers()->saveMany(factory(Answer::class, rand(1,5))->make());
-              });
-        });
+        $this->call([
+            UsersQuestionsAnswersTableSeeder::class,
+            FavoritesTableSeeder::class,
+        ]);
     }
 }
--- database/seeds/UsersQuestionsAnswersTableSeeder.php
+++ database/seeds/UsersQuestionsAnswersTableSeeder.php
@@ -11,6 +11,18 @@ class UsersQuestionsAnswersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \DB::table('answers')->delete();
+        \DB::table('questions')->delete();
+        \DB::table('users')->delete();
+
+        factory(User::class, 3)->create()->each(function($u) {
+            $u->questions()
+              ->saveMany(
+                  factory(Question::class, rand(1,5))->make()
+              )
+              ->each(function($q) {
+                $q->answers()->saveMany(factory(Answer::class, rand(1,5))->make());
+              });
+        });
     }
 }
