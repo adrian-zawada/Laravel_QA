36e784ba47153503f2928784304319927ed2a084
adrian-zawada <azawada93@gmail.com>
2020-01-10 19:56:52 +0100

    Build a store and destroy method for FavoritesController

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+    public function store(Question $question)
+    {
+        $question->favorites()->attach(auth()->id());
+
+        return back();
+    }
+
+    public function destroy(Question $question)
+    {
+        $question->favorites()->detach(auth()->id());
+
+        return back();
+    }
+}
