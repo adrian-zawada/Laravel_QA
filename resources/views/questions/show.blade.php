01f401729b9472b1dda43523689c321ae2e11d2c
adrian-zawada <azawada93@gmail.com>
2020-01-10 20:11:55 +0100

    Fix small bug

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -28,7 +28,8 @@
                             </a>
                             <a title="Click to mark as favorite question (Click again to undo)"
                                 class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
-                                onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();">
+                                onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
+                                >
                                 <i class="fas fa-star fa-2x"></i>
                                 <span class="favorites-count">{{ $question->favorites_count }}</span>
                             </a>
