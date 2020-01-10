599093267bb33ada85a78a8a7b57326c5eb9802d
adrian-zawada <azawada93@gmail.com>
2020-01-10 19:58:56 +0100

    Create a middleware for FavoritesController

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class FavoritesController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(Question $question)
     {
         $question->favorites()->attach(auth()->id());
