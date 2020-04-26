d6d0c2726fe6b9c1980369a7e336b9b3e26fe58a
Adrian Zawada <azawada93@gmail.com>
2020-04-26 18:20:40 +0200

    Run npm update and compiling js file and scss

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,21 @@
  */
 
 require('./bootstrap');
-// require('./fontawesome');
+
+//Font Awesome
+import { config, library, dom } from '@fortawesome/fontawesome-svg-core';
+config.autoReplaceSvg = 'nest';
+
+import { faCaretUp,
+    faCaretDown,
+    faStar,
+    faCheck } from '@fortawesome/free-solid-svg-icons';
+   library.add(faCaretUp,
+    faCaretDown,
+    faStar,
+    faCheck);
+   // Kicks off the process of finding <i> tags and replacing with <svg>
+   dom.watch();
 
 window.Vue = require('vue');
 
