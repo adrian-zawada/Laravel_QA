b1d713509cc05902affe2cef0bbe31360e4efcf8
Adrian Zawada <azawada93@gmail.com>
2020-04-26 21:41:15 +0200

    Update answer record by vue component

--- resources/views/answers/_answer.blade.php
+++ resources/views/answers/_answer.blade.php
@@ -1,29 +1,40 @@
-<div class="media post">
-    @include('shared._vote', [
-        'model' => $answer
-    ])
-    <div class="media-body">
-        {!! $answer->body_html !!}
-        <div class="row">
-            <div class="col-4">
-                <div class="ml-auto">
-                    @can ('update', $answer)
-                        <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                    @endif
-
-                    @can('delete', $answer)
-                        <form class="form-delete" method="POST" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
-                            @method('DELETE')
-                            @csrf
-                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">DELETE</button>
-                        </form>
-                    @endcan
+<answer :answer="{{ $answer }}" inline-template>
+    <div class="media post">
+        @include('shared._vote', [
+            'model' => $answer
+        ])
+        <div class="media-body">
+            <form v-if="editing" @submit.prevent="update">
+                <div class="form-group">
+                    <textarea v-model="body" rows="10" class="form-control"></textarea>
                 </div>
-            </div>
-            <div class="col-4"></div>
-            <div class="col-4">
-                <user-info :model="{{ $answer }}" label="Asked"></user-info>
+                <button class="btn btn-primary">Update</button>
+                <button class="btn btn-outline-secondary" type="button" @click="cancel">Cancel</button>
+            </form>
+            <div v-else>
+                <div v-html="bodyHtml"></div>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="ml-auto">
+                            @can ('update', $answer)
+                                <a @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>
+                            @endif
+
+                            @can('delete', $answer)
+                                <form class="form-delete" method="POST" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">DELETE</button>
+                                </form>
+                            @endcan
+                        </div>
+                    </div>
+                    <div class="col-4"></div>
+                    <div class="col-4">
+                        <user-info :model="{{ $answer }}" label="Asked"></user-info>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</answer>
