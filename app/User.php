0b733e0ea78d140da1f56e38bf1207d26c3d3b23
Adrian Zawada <azawada93@gmail.com>
2020-12-01 23:12:01 +0100

    Delete unnecessary facades

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Policies\QuestionPolicy;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
