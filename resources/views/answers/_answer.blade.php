ff069d5840371ada07bf40aab64769eb74d72a41
Adrian Zawada <azawada93@gmail.com>
2020-12-03 00:14:03 +0100

    Create Answers component and implement vue list rendering

--- resources/views/answers/_answer.blade.php
+++ resources/views/answers/_answer.blade.php
@@ -1,36 +1,3 @@
 <answer :answer="{{ $answer }}" inline-template>
-    <div class="media post">
-        <vote :model="{{ $answer }}" name="answer"></vote>
-
-        <div class="media-body">
-            <form v-if="editing" @submit.prevent="update">
-                <div class="form-group">
-                    <textarea v-model="body" rows="10" class="form-control" required></textarea>
-                </div>
-                <button class="btn btn-primary" :disabled="isInvalid">Update</button>
-                <button class="btn btn-outline-secondary" type="button" @click="cancel">Cancel</button>
-            </form>
-            <div v-else>
-                <div v-html="bodyHtml"></div>
-                <div class="row">
-                    <div class="col-4">
-                        <div class="ml-auto">
-                            @can ('update', $answer)
-                                <a @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>
-                            @endif
-
-                            @can('delete', $answer)
-                            <button @click="destroy" class="btn btn-sm btn-outline-danger">DELETE</button>
-
-                            @endcan
-                        </div>
-                    </div>
-                    <div class="col-4"></div>
-                    <div class="col-4">
-                        <user-info :model="{{ $answer }}" label="Asked"></user-info>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 </answer>
