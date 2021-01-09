61b51de50c5f453b504e304e0b37de8f6ba0bc9e
Adrian Zawada <azawada93@gmail.com>
2021-01-09 02:03:59 +0100

    Create a Question component and convert the blade syntax to Vue.js syntax

---
+++ resources/js/components/Question.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <div class="d-flex algin-items-center">
+                            <h1>{{ title }}</h1>
+                            <div class="ml-auto">
+                                <a href="/questions" class="btn btn-outline-secondary">Back to All Question</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="media">
+                    <vote :model="question" name="question"></vote>
+
+                        <div class="media-body">
+                            <div v-html="bodyHtml"></div>
+                            <div class="row">
+                                <div class="col-4"></div>
+                                <div class="col-4"></div>
+                                <div class="col-4">
+                                    <user-info :model="question" label="Asked"></user-info>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['question'],
+
+    data () {
+        return {
+            title: this.question.title,
+            body: this.question.body,
+            bodyHtml: this.question.body_html
+        }
+    }
+}
+</script>
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -2,39 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="card-title">
-                        <div class="d-flex algin-items-center">
-                            <h1>{{ $question->title }}</h1>
-                            <div class="ml-auto">
-                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to All Question</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <hr>
-
-                    <div class="media">
-                    <vote :model="{{ $question }}" name="question"></vote>
-
-                        <div class="media-body">
-                            {!! $question->body_html !!}
-                            <div class="row">
-                                <div class="col-4"></div>
-                                <div class="col-4"></div>
-                                <div class="col-4">
-                                    <user-info :model="{{ $question }}" label="Asked"></user-info>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <question :question="{{ $question }}"></question>
     <answers :question="{{ $question }}"></answers>
 </div>
 @endsection
